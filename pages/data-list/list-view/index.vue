021e94de55be2baa4a6341a048af6f85b1f0359b
BiLosxD <billyrosssta.maria@gmail.com>
2020-12-16 15:59:39 +0800

    doing the list table

--- pages/data-list/list-view/index.vue
+++ pages/data-list/list-view/index.vue
@@ -1,6 +1,27 @@
 <template lang="html">
     <div id="dashboard">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <table class="table list_view">
+            <thead>
+                <tr>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                    <th>Test</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(data, key) in 10" :key="key">
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                    <td>Test</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
