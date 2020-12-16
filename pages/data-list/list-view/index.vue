36ac75181fb471b41ba634174503b4a8123dbe7f
BiLosxD <billyrosssta.maria@gmail.com>
2020-12-16 19:38:58 +0800

    ongoing update

--- pages/data-list/list-view/index.vue
+++ pages/data-list/list-view/index.vue
@@ -8,7 +8,7 @@
                     <th>Test</th>
                     <th>Test</th>
                     <th>Test</th>
-                    <th>Test</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -18,7 +18,14 @@
                     <td>Test</td>
                     <td>Test</td>
                     <td>Test</td>
-                    <td>Test</td>
+                    <td class="actions">
+                        <div class="item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#7A7A7A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit w-5 h-5 hover:text-primary stroke-current"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                        </div>
+                        <div class="item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#7A7A7A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash w-5 h-5 hover:text-danger stroke-current"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+                        </div>
+                    </td>
                 </tr>
             </tbody>
         </table>
