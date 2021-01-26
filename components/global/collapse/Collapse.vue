9fa8767db079f35e682a06ec9271bbe68346c648
BiLosxD <billyrosssta.maria@gmail.com>
2021-01-26 18:00:40 +0800

    created a global accordion

---
+++ components/global/collapse/Collapse.vue
@@ -0,0 +1,53 @@
+<template lang="html">
+    <div id="collapse">
+        <collapse-item :item="item" v-for="(item, key) in populateItems" :key="key" />
+    </div>
+</template>
+
+<script>
+    import CollapseItem from '~/components/global/collapse/CollapseItem'
+
+    export default {
+        components: {
+            CollapseItem
+        },
+        props: {
+            items: {
+                type: Object/Array,
+                default: () => {
+                    return [
+                        {
+                            name: 'Collapse',
+                            description: 'The quick brown fox jumps over the lazy dog.',
+                            open: false
+                        }
+                    ]
+                }
+            }
+        },
+        computed: {
+            populateItems () {
+                const me = this,
+                    results = []
+
+                me.items.forEach((item, ket) => {
+                    results.push(item)
+                })
+
+                return results
+            }
+        },
+        methods: {
+            checkItems (data) {
+                const me = this
+                me.items.forEach((item, key) => {
+                    if (data.id != item.id) {
+                        let target = document.getElementById(`item_${item.id}`)
+                        item.open = false
+                        target.querySelector('.description').style.height = '0px'
+                    }
+                })
+            }
+        }
+    }
+</script>
--- pages/components/collapse/index.vue
+++ pages/components/collapse/index.vue
@@ -1,14 +1,81 @@
 <template lang="html">
-    <div id="dashboard">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div id="dashboard" v-if="loaded">
+
+        <div class="box mb">
+            <div class="top_box">
+                <h2>Default</h2>
+            </div>
+            <div class="bottom_box">
+                <collapse :items="items" />
+            </div>
+        </div>
+
     </div>
 </template>
 
 <script>
+    import Collapse from '~/components/global/collapse/Collapse'
+
     export default {
+        components: {
+            'collapse': Collapse
+        },
+        data () {
+            return {
+                loaded: false,
+                items: [
+                    {
+                        id: 0,
+                        name: 'Collapse',
+                        description: 'The quick brown fox jumps over the lazy dog.',
+                        open: false
+                    },
+                    {
+                        id: 1,
+                        name: 'Collapse',
+                        description: 'The quick brown fox jumps over the lazy dog.',
+                        open: false
+                    },
+                    {
+                        id: 2,
+                        name: 'Collapse',
+                        description: 'The quick brown fox jumps over the lazy dog.',
+                        open: false
+                    },
+                    {
+                        id: 3,
+                        name: 'Collapse',
+                        description: 'The quick brown fox jumps over the lazy dog.',
+                        open: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            initialization (event) {
+                const me = this
+                if (document.readyState != 'interactive') {
+                    setTimeout( () => {
+                        me.$store.commit('global/loader/checkLoader', { status: false })
+                        me.loaded = true
+                        document.body.classList.remove('no_scroll', 'no_click')
+                    }, 1000)
+                }
+            }
+        },
         mounted () {
             const me = this
-            me.$store.commit('global/settings/populateTitle', { title: 'Collapse' })
+            me.initialization()
+        },
+        asyncData ({ store }) {
+            store.commit('global/settings/populateTitle', { title: 'Collapse' })
+            store.commit('global/loader/checkLoader', { status: true })
+        },
+        beforeMount () {
+            window.addEventListener('load', this.initialization)
+        },
+        beforeDestroy () {
+            window.removeEventListener('load', this.initialization)
         }
     }
 </script>
