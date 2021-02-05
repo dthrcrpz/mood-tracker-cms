f251286e7604e69aee253d124eb7ff242e21e537
BiLosxD <billyrosssta.maria@gmail.com>
2021-02-05 17:59:04 +0800

    refactored adshboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,11 +1,41 @@
 <template lang="html">
-    <div id="dashboard">
+    <div id="dashboard" v-if="loaded">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
     </div>
 </template>
 
 <script>
     export default {
-
+        data () {
+            return {
+                loaded: false
+            }
+        },
+        methods: {
+            initialization (event) {
+                const me = this
+                if (document.readyState != 'interactive') {
+                    setTimeout( () => {
+                        me.$store.commit('global/loader/checkLoader', { status: false })
+                        me.loaded = true
+                        document.body.classList.remove('no_scroll', 'no_click')
+                    }, 1000)
+                }
+            }
+        },
+        mounted () {
+            const me = this
+            me.initialization()
+        },
+        asyncData ({ store }) {
+            store.commit('global/settings/populateTitle', { title: 'Dashboard' })
+            store.commit('global/loader/checkLoader', { status: true })
+        },
+        beforeMount () {
+            window.addEventListener('load', this.initialization)
+        },
+        beforeDestroy () {
+            window.removeEventListener('load', this.initialization)
+        }
     }
 </script>
