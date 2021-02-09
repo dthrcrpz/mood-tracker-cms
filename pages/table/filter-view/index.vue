fc949ba2a495a56ea11e6d6f84c5f269b4e62993
BiLosxD <billyrosssta.maria@gmail.com>
2021-02-09 13:45:57 +0800

    added filter view and action button

--- pages/table/filter-view/index.vue
+++ pages/table/filter-view/index.vue
@@ -1,6 +1,13 @@
 <template lang="html">
     <div id="dashboard" v-if="loaded">
 
+        <div class="actions">
+            <div class="primary_button pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                <span>Add Content</span>
+            </div>
+        </div>
+
         <div class="action_filter pointer" @click="filter = true">
             <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#FFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-filter"><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"></polygon></svg>
         </div>
