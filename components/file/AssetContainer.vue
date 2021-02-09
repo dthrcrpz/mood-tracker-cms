62956e0b9bddd3181649c188fcc305e237c2b4bf
BiLosxD <billyrosssta.maria@gmail.com>
2021-02-09 16:33:34 +0800

    added single file and image input

---
+++ components/file/AssetContainer.vue
@@ -0,0 +1,160 @@
+<template lang="html">
+    <div class="group">
+        <div class="file">
+            <input type="file" name="file[]" :id="`files${unique}`" :multiple="multiple" :accept="populateExtensions" class="o_hidden" @change="getFiles($event)" v-validate="{required: true, image: (type == 'image') ? true : false, size: 500000, ext: extensions}">
+            <label :for="`files${unique}`" @dragover.prevent @dragenter.prevent @drop.prevent="dropFiles($event)">
+                <p>Drag a {{ type }} here<br>
+                or <b class="pointer">browse</b> to upload.</p>
+            </label>
+            <transition name="slide"><span class="validate" v-if="errors.has('file[]')">{{ properFormat(errors.first('file[]')) }}</span></transition>
+        </div>
+        <div class="preview_image" v-if="files.length > 0">
+            <div :class="[ 'preview', (type == 'file') ? 'big' : '' ]" v-for="(data, key) in populateFiles" :key="key">
+                <img src="" :id="`preview_image${key}`" v-if="type == 'image'" />
+                <div class="close" @click="removeTempFiles(key)"></div>
+                <div class="item">
+                    <span v-line-clamp="1">{{ data.name }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="disclaimer">
+            <div class="violator">Drag and drop {{ type }}.<br>
+            Maximum, file size is 50 mb<br>
+            Supported file formats: {{ populateExtensions }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            unique: {
+                type: Number,
+                default: 0
+            },
+            multiple: {
+                type: Boolean,
+                default: true
+            },
+            type: {
+                type: String,
+                default: 'image'
+            },
+            extensions: {
+                type: Array/Object,
+                default: ['jpeg', 'jpg', 'png']
+            },
+            limit: {
+                type: Number,
+                default: 1
+            }
+        },
+        data () {
+            return {
+                files: []
+            }
+        },
+        computed: {
+            populateFiles () {
+                const me = this
+                let result
+                result = me.files
+                return result
+            },
+            populateExtensions () {
+                const me = this
+                let result = ''
+                me.extensions.forEach((item, key) => {
+                    if (key != (me.extensions.length - 1)) {
+                        result += `.${item}, `
+                    } else {
+                        result += `.${item}`
+                    }
+                })
+
+                return result
+            }
+        },
+        methods: {
+            removeTempFiles (unique) {
+                const me = this,
+                    data_transfer = new DataTransfer(),
+                    target_file = document.getElementById(`files${me.unique}`),
+                    { files } = target_file
+
+                for (let i = 0; i < files.length; i++) {
+                    const file = files[i]
+                    if (unique !== i) data_transfer.items.add(file)
+                    target_file.files = data_transfer.files
+                    me.files = target_file.files
+                }
+
+                if (me.type == 'image') {
+                    let element = document.getElementById(`files${me.unique}`)
+                    for (let i = 0; i < element.files.length; i++) {
+
+                        let reader = new FileReader()
+                        reader.onload = function () {
+                            let image = document.getElementById(`preview_image${i}`)
+                            image.src = reader.result
+                        }
+                        reader.readAsDataURL(element.files[i])
+                    }
+                }
+            },
+            /**
+             * manipulate files
+             */
+            getFiles (event) {
+                const me = this
+                let element = document.getElementById(`files${me.unique}`)
+
+                if (event.target.files.length > 0) {
+                    me.files = []
+                    for (let i = 0; i < element.files.length; i++) {
+
+                        me.files.push(element.files[i])
+
+                        if (me.type == 'image') {
+                            let reader = new FileReader()
+                            reader.onload = function () {
+                                let image = document.getElementById(`preview_image${i}`)
+                                image.src = reader.result
+                            }
+                            reader.readAsDataURL(element.files[i])
+                        }
+                    }
+                }
+            },
+            /**
+             * drag and drop event files
+             * @param  {[object]} event [event handler]
+             */
+            dropFiles (event) {
+                const me = this
+
+                let target = event.dataTransfer.files,
+                    target_file = document.getElementById(`files${me.unique}`)
+
+                const data_transfer = new DataTransfer()
+                let length = (me.multiple) ? target.length : 1
+
+                for (let i = 0; i < length; i++) {
+                    data_transfer.items.add(target[i])
+
+                    target_file.files = data_transfer.files
+                    me.files = target_file.files
+
+                    if (me.type == 'image') {
+                        let reader = new FileReader()
+                        reader.onload = function () {
+                            let image = document.getElementById(`preview_image${i}`)
+                            image.src = reader.result
+                        }
+                        reader.readAsDataURL(target[i])
+                    }
+                }
+            }
+        }
+    }
+</script>
--- pages/form-elements/file/index.vue
+++ pages/form-elements/file/index.vue
@@ -5,11 +5,25 @@
             <div class="top_box">
                 <h2>Default</h2>
                 <div class="description">
-                    <p>You can choose if file is multiple or single by adding <b>:multiple</b> property with a value of <b>TRUE</b> or <b>FALSES</b>.</p>
+                    <p>You can choose if file is multiple or single by adding <b>:multiple</b> property with a value of <b>TRUE</b> or <b>FALSE</b>.</p><br />
+                    <p>Property <b>:multiple="true"</b>, <b>:type="image"</b> and <b>:extensions="['jpeg', 'jpg', 'png']"</b></p>
+                    <!-- <p>You can change the type of asset by adding <b>:type</b> property with a value of <b>IMAGE</b> or <b>FILE</b>.</p> -->
                 </div>
             </div>
             <div class="bottom_box form">
-                <file-container />
+                <asset-container />
+            </div>
+        </div>
+
+        <div class="box mb">
+            <div class="top_box">
+                <h2>Single File</h2>
+                <div class="description">
+                    <p>Property <b>:multiple="false"</b>, <b>:type="file"</b> and <b>:extensions="['pdf', 'txt', 'doc']"</b></p>
+                </div>
+            </div>
+            <div class="bottom_box form">
+                <asset-container :unique="1" :extensions="['pdf', 'txt', 'doc']" :multiple="false" :type="'file'" />
             </div>
         </div>
 
@@ -17,11 +31,11 @@
 </template>
 
 <script>
-    import FileContainer from '~/components/file/FileContainer'
+    import AssetContainer from '~/components/file/AssetContainer'
 
     export default {
         components: {
-            FileContainer
+            'assetContainer': AssetContainer
         },
         data () {
             return {
