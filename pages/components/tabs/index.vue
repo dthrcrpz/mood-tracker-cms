77f2cb7e8e5b313246246b464f0b4f11968e5ead
BiLosxD <billyrosssta.maria@gmail.com>
2021-02-11 15:04:11 +0800

    added tabs

--- pages/components/tabs/index.vue
+++ pages/components/tabs/index.vue
@@ -6,7 +6,30 @@
                 <h2>Default</h2>
             </div>
             <div class="bottom_box">
-                <tab />
+                <div id="tabs">
+                    <div class="parent">
+                        <ul>
+                            <li :class="[ 'pointer', (active_tab == data.unique) ? 'active' : '' ]" v-for="(data, key) in tabs" :key="key" @click="getTab(data)">{{ data.name }}</li>
+                        </ul>
+                    </div>
+                    <div class="child">
+                        <div v-if="active_tab == 0">
+                            <div class="fade">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                            </div>
+                        </div>
+                        <div v-if="active_tab == 1">
+                            <div class="fade">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+                            </div>
+                        </div>
+                        <div v-if="active_tab == 2">
+                            <div class="fade">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -14,18 +37,32 @@
 </template>
 
 <script>
-    import Tab from '~/components/tabs/Tab'
-
     export default {
-        components: {
-            Tab
-        },
         data () {
             return {
-                loaded: false
+                loaded: false,
+                tabs: [
+                    {
+                        name: 'Details',
+                        unique: 0
+                    },
+                    {
+                        name: 'Materials',
+                        unique: 1
+                    },
+                    {
+                        name: 'Videos',
+                        unique: 2
+                    }
+                ],
+                active_tab: 0
             }
         },
         methods: {
+            getTab (data) {
+                const me = this
+                me.active_tab = data.unique
+            },
             initialization (event) {
                 const me = this
                 if (document.readyState != 'interactive') {
