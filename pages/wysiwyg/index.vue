77124240e8828c39b3011b8a6bf7125a45d2e9c4
BiLosxD <billyrosssta.maria@gmail.com>
2021-03-16 14:17:02 +0800

    ongoing wysiwyg

---
+++ pages/wysiwyg/index.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+    <div id="dashboard" v-if="loaded">
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                loaded: false
+            }
+        },
+        methods: {
+            initialization (event) {
+                const me = this
+                if (document.readyState != 'interactive') {
+                    setTimeout( () => {
+                        me.$store.commit('global/loader/checkLoader', { status: false })
+                        me.loaded = true
+                        document.body.classList.remove('no_scroll', 'no_click')
+                    }, 1000)
+                }
+            }
+        },
+        mounted () {
+            const me = this
+            me.initialization()
+        },
+        asyncData ({ store }) {
+            store.commit('global/settings/populateTitle', { title: 'Wysiwyg' })
+            store.commit('global/loader/checkLoader', { status: true })
+        },
+        beforeMount () {
+            window.addEventListener('load', this.initialization)
+        },
+        beforeDestroy () {
+            window.removeEventListener('load', this.initialization)
+        }
+    }
+</script>
