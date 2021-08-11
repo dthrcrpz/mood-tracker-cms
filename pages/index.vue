bb34a8f9593e3315f53d4f4a0c1a56fdc1cfbe39
BiLosxD <billyrosssta.maria@gmail.com>
2021-08-11 14:31:55 +0800

    fixed something

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
                                 <transition name="slide"><span class="validate" v-if="errors.length > 0">{{ errors[0] }}</span></transition>
                             </ValidationProvider>
                             <div class="buttons">
-                                <a href="https://bit-interchange.com" class="primary_button outline pointer">Back To Home</a>
+                                <a href="#" class="primary_button outline pointer">Back To Home</a>
                                 <button type="submit" class="primary_button pointer">Login</button>
                             </div>
                         </form>
