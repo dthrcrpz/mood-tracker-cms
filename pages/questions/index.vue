642074eee3f62b37e4ca34c853b2e52316101941
BiLosxD <billyrosssta.maria@gmail.com>
2021-11-03 22:29:14 +0800

    added scoring settings

--- pages/questions/index.vue
+++ pages/questions/index.vue
@@ -47,7 +47,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                                 <span>Edit</span>
                             </nuxt-link>
-                            <div class="item ml delete pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `/questions/${data.id}`, item_type: 'question' } })">
+                            <div class="item ml delete pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `questions/${data.id}`, item_type: 'question' } })">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
                                 <span>Delete</span>
                             </div>
